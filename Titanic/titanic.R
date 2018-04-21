1665efde5e45e049ae335a066c35ff39b6bb463b
Marcos001 <santosMsantos01@gmail.com>
2018-04-21 01:05:59 -0300

    Análise dos Dados

--- Titanic/titanic.R
+++ Titanic/titanic.R
@@ -1,3 +1,25 @@
 
 # carregando as bibliotecas
-library('ggplot2')
+library('ggplot2') # visualição
+library('ggthemes') # visualization
+library('scales') # visualization
+library('dplyr') # data manipulation
+library('mice') # imputation
+library('randomForest') # classification algorithm
+
+train <- read.csv('data/train.csv', stringsAsFactors = F)
+test <- read.csv('data/test.csv', stringsAsFactors = F)
+
+full  <- bind_rows(train, test) # junta os dados de treinamento e teste
+
+#checa os dados
+str(full)
+
+# Engenharia de Recursos
+print('-------------------------------------------------\n')
+
+# Pega o título dos nomes dos passageiros
+full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
+
+# Mostrar contagem de títulos por sexo
+table(full$Sex, full$)
