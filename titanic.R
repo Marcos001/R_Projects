6825bc0725b8677f9b5e7550520cc3426c3010ee
Marcos001 <santosMsantos01@gmail.com>
2018-05-22 16:18:26 -0300

    titanic prediction with R

--- titanic.R
+++ titanic.R
@@ -1,7 +1,9 @@
 
+caminho <- "/home/marcos/codes/R_Projects/Titanic/data/"
+
 # read databases
-train <- read.csv('/media/nig/Data/Titanic/train.csv')
-test <- read.csv('/media/nig/Data/Titanic/test.csv')
+train <- read.csv(paste(caminho,'train.csv', sep = ''))
+test <- read.csv(paste(caminho,'test.csv', sep = ''))
 
 # devolve as colunas de um dataframe
 names(train)
@@ -36,6 +38,26 @@ prop.table(table(criancas$Survived))
 library(rpart)
 
 arvore <- rpart(Survived ~ Age + Sex + Pclass + Fare, data=train, method="class")
+
+## representação de arvore ruim
+print(arvore)
+
+## bibliotecas graficas para representar uma arvore de decisao
+library(rattle)
+library(RColorBrewer)
+## plota a arvore
+fancyRpartPlot(arvore)
+
+# usar a predição da arvore de decisão e fazer o agrupamento dos dados
+test$Survived <- 0
+submissao = data.frame(PassengerId=test$PassengerId, Survived=test$Survived)
+
+previsao = predict(arvore, test, type = "class")
+
+submissao$Survived = previsao
+
+write.csv(submissao, file = 'predictions/r_submission_tictanic.csv', row.names=FALSE)
+
 
 # source
 ## http://blog.caelum.com.br/r-titanic-e-data-science/ ##
