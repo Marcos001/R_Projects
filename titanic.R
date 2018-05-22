0483cef44a5b41a17026a91089f257727b5d8ae3
Nigon001 <ferreiraviniciussdi@gmail.com>
2018-05-22 08:17:32 -0300

    apredendo R no conjundo de dados Titanic

--- titanic.R
+++ titanic.R
@@ -12,9 +12,33 @@ table(train$Survived)
 # vendo a porcentagens de passageiros que mortes e sobreviventes
 prop.table(table(train$Survived))
 
+# analisando dados entre homens e mulheres
 homens <- train[train$Sex == 'male',]
 mulheres <- train[train$Sex == 'female',]
 
 # vendo a porcetagem de homens e mulheres que sobreviveram
 prop.table(table(homens$Survived))
 prop.table(table(mulheres$Survived))
+
+# analisando os pasageiros com idade inferior a 5 anos
+## corrigindo campus em nulos da idade
+passageiros_com_idade <- train[!is.na(train$Age),]
+idade_media <- mean(passageiros_com_idade$Age)
+
+## preencendo as colunas mulas com a media de idades
+train[is.na(train$Age),]$Age <- idade_media
+
+criancas <- train[train$Age < 5, ]
+
+## sera que a maioria das criancas foram salvas do desastre
+prop.table(table(criancas$Survived))
+
+library(rpart)
+
+arvore <- rpart(Survived ~ Age + Sex + Pclass + Fare, data=train, method="class")
+
+# source
+## http://blog.caelum.com.br/r-titanic-e-data-science/ ##
+
+
+
